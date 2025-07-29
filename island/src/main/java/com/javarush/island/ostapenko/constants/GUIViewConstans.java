42bd5dc76ba74a3757e91dad04dee612bf61ef80
Darfresor <vectnik@gmail.com>
2025-07-29 06:54:42 +0300

    добавлена простая отрисовка клеток через gridpanel

--- island/src/main/java/com/javarush/island/ostapenko/constants/GUIViewConstans.java
+++ island/src/main/java/com/javarush/island/ostapenko/constants/GUIViewConstans.java
@@ -1,4 +1,4 @@
-package com.javarush.island.ostapenko.constans;
+package com.javarush.island.ostapenko.constants;
 
 public class GUIViewConstans {
     public static final String WINDOW_NAME = "Остров";
--- island/src/main/java/com/javarush/island/ostapenko/view/GUIView.java
+++ island/src/main/java/com/javarush/island/ostapenko/view/GUIView.java
@@ -1,12 +1,17 @@
 package com.javarush.island.ostapenko.view;
 
 import com.javarush.island.ostapenko.entity.Result;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-import static com.javarush.island.ostapenko.constans.GUIViewConstans.*;
-
+import static com.javarush.island.ostapenko.constants.GUIViewConstans.*;
+/**
+ * Class GUIView use JavaFx library.
+ * Contains interface rendering and event handling.
+ */
 public class GUIView implements View{
     Stage stage;
     public GUIView(Stage stage) {
@@ -20,10 +25,38 @@ public class GUIView implements View{
     }
 
     private void initUI() {
+
+        GridPane grid = new GridPane();
+        grid.setGridLinesVisible(true);
+
+        for (int i = 0; i < 20; i++) {
+            ColumnConstraints column = new ColumnConstraints();
+            column.setHgrow(Priority.ALWAYS);
+            grid.getColumnConstraints().add(column);
+        }
+        for (int i = 0; i < 20; i++) {
+            RowConstraints row = new RowConstraints();
+            row.setVgrow(Priority.ALWAYS);
+            grid.getRowConstraints().add(row);
+        }
+
+        // Заполнение ячеек
+        for (int row = 0; row < 20; row++) {
+            for (int col = 0; col < 20; col++) {
+                Label cell = new Label("R" + row + " C" + col);
+                cell.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+                cell.setStyle("-fx-border-color: black; -fx-alignment: CENTER;");
+                grid.add(cell, col, row);
+            }
+        }
+
+        Scene scene = new Scene(grid, 400, 400);
         stage.setTitle(WINDOW_NAME);
-        BorderPane root = new BorderPane();
-        Scene scene = new Scene(root, 1800, 1000);
         stage.setScene(scene);
+        stage.show();
+
+
+
     }
 
     @Override
