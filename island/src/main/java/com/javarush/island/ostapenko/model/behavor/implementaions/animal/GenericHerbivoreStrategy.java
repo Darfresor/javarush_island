662adc3c6eb75b9f1b81174f7297ffb7b123896d
Darfresor <vectnik@gmail.com>
2025-08-19 13:30:45 +0300

    доработано питание травоядных с учетом событий в отдельных потоках

--- island/src/main/java/com/javarush/island/ostapenko/model/behavor/implementaions/animal/GenericHerbivoreStrategy.java
+++ island/src/main/java/com/javarush/island/ostapenko/model/behavor/implementaions/animal/GenericHerbivoreStrategy.java
@@ -33,7 +33,7 @@ public class GenericHerbivoreStrategy implements Eatable {
                 if (ThreadLocalRandom.current().nextDouble() < probability) {
                     Logger.logFeedingService(eater, cell, String.format("%s съел %s",
                             eater.getSpeciesName(), target.getSpeciesName()));
-                    mediator.notify(new PlantEatenEvent(eater, target, cell));
+                    modelThreadPoolManager.executeDeathTask(()->mediator.notify(new PlantEatenEvent(eater, target, cell)));
                     eater.setSatiety(calculateSatiety(eater, target));
                     Logger.logFeedingService(eater, cell, String.format("Сытость %s = %f",
                             eater.getSpeciesName(), eater.getSatiety()));
@@ -41,7 +41,7 @@ public class GenericHerbivoreStrategy implements Eatable {
                 } else {
                     Logger.logFeedingService(eater, cell, String.format("%s не смог съесть %s",
                             eater.getSpeciesName(), target.getSpeciesName()));
-                    if (checkDeathByStarvation(eater, cell, island)) return;
+                    if (checkDeathByStarvation(eater, cell, island, modelThreadPoolManager)) return;
                     return;
                 }
 
@@ -49,16 +49,16 @@ public class GenericHerbivoreStrategy implements Eatable {
         }
         Logger.logFeedingService(eater, cell, String.format("%s не нашел еды в этой клетке",
                 eater.getSpeciesName()));
-        if (checkDeathByStarvation(eater, cell, island)) return;
-        mediator.notify(new AnimalMoveForEatEvent(eater, cell, island));
+        if (checkDeathByStarvation(eater, cell, island, modelThreadPoolManager)) return;
+        modelThreadPoolManager.executeMoveTask(()->mediator.notify(new AnimalMoveForEatEvent(eater, cell, island)));
     }
 
-    private boolean checkDeathByStarvation(Animal eater, Cell cell, Island island) {
+    private boolean checkDeathByStarvation(Animal eater, Cell cell, Island island, ModelThreadPoolManager modelThreadPoolManager) {
         reduceSatiety(eater);
         Logger.logFeedingService(eater, cell, String.format("Сытость %s = %f",
                 eater.getSpeciesName(), eater.getSatiety()));
         if (eater.getSatiety() == 0) {
-            mediator.notify(new AnimalStarvationEvent(eater, cell, island));
+            modelThreadPoolManager.executeDeathTask(()->mediator.notify(new AnimalStarvationEvent(eater, cell, island)));
             return true;
         }
         return false;
